83a794907a379c885d547725b0116bf738264fef
Faheem Hassan <51878457+faheem102@users.noreply.github.com>
2023-10-30 01:10:00 +0100

    complete anlog clock

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("hello world");
+const secondHand = document.querySelector('.second');
+  const minsHand = document.querySelector('.minute');
+  const hourHand = document.querySelector('.hour');
+setInterval(() => {
+    const currentDate = new Date();
+    const currentHour = currentDate.getHours();
+    const currentMinute = currentDate.getMinutes();
+    const currentSecond = currentDate.getSeconds();
+    
+    currentHour_rotation = 30 * currentHour + currentMinute / 2; //converting current time
+    currentMinute_rotation = 6 * currentMinute;
+    currentSecond_rotation = 6 * currentSecond;
+    
+    hourHand.style.transform = `rotate(${currentHour_rotation}deg)`;
+    minsHand.style.transform = `rotate(${currentMinute_rotation}deg)`;
+    secondHand.style.transform = `rotate(${currentSecond_rotation}deg)`;
+    }, 1000);
